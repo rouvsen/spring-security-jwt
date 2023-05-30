c47cbb4b0f32efdb9bb4b785b4c5d349cce67a8a
rovshan <rouvsen@gmail.com>
2023-05-30 18:16:07 +0400

    User Model added

---
+++ src/main/java/com/rouvsen/springsecurityjwt/model/User.java
@@ -0,0 +1,35 @@
+package com.rouvsen.springsecurityjwt.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
