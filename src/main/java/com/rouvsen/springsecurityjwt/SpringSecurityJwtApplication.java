32f250b44f44ecc0f1e2a0929d6fb77393d9c7c2
rovshan <rouvsen@gmail.com>
2023-05-30 17:56:28 +0400

    changes

--- src/main/java/com/rouvsen/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/rouvsen/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class SpringSecurityJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
