ac41a4ba9d5a19942650b88fc168fb71a6149807
rovshan <rouvsen@gmail.com>
2023-05-30 17:52:02 +0400

    configs added

--- src/main/java/com/rouvsen/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/rouvsen/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @SpringBootApplication
 @EnableWebSecurity
 public class SpringSecurityJwtApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
