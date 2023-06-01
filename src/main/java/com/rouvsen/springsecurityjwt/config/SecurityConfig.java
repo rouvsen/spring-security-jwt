7839fe06c77e963ec696e62d966425afe54e3de3
rovshan <rouvsen@gmail.com>
2023-06-01 12:31:21 +0400

    Cors configs, security ignoring for some endpoints, SFC(Matchers) configs

---
+++ src/main/java/com/rouvsen/springsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.rouvsen.springsecurityjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .cors().and()//for activating cors configs, we should call cors()
+                .authorizeRequests((auth) -> {
+                    auth.antMatchers("/api/admin").hasAuthority("ADMIN");
+                    auth.antMatchers("/api/user").hasAnyAuthority("ADMIN", "USER");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers(
+                "/api/public",
+                "/api/auth/login",
+                "/api/images/**",
+                "/h2-console/**"));
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")//all end-points
+                        .allowedMethods("*");//all methods
+            }
+        };
+    }
+}
