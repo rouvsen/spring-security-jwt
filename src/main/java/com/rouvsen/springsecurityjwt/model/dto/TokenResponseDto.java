cb7f497cd768e48b166c70f204f9ca154a6ccf7a
rovshan <rouvsen@gmail.com>
2023-06-02 00:44:23 +0400

    accessToken and Dto - details coming while /auth/login in process

---
+++ src/main/java/com/rouvsen/springsecurityjwt/model/dto/TokenResponseDto.java
@@ -0,0 +1,15 @@
+package com.rouvsen.springsecurityjwt.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponseDto {
+    private String accessToken;
+    private UserDto userDto;
+}
