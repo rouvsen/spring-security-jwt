5d3265bc2ceb89c7f3414a891dde4a9f96f9723c
rovshan <rouvsen@gmail.com>
2023-06-01 17:49:56 +0400

    ObjectMapper Bean added to Config

--- src/main/java/com/rouvsen/springsecurityjwt/config/BeanConfig.java
+++ src/main/java/com/rouvsen/springsecurityjwt/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.rouvsen.springsecurityjwt.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,4 +14,8 @@ public class BeanConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
