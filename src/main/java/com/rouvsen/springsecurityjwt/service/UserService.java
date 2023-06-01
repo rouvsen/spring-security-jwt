bb7ba5141b048e8fe7b8dc4c208925c18aed0e50
rovshan <rouvsen@gmail.com>
2023-06-01 12:34:40 +0400

    UserService added

---
+++ src/main/java/com/rouvsen/springsecurityjwt/service/UserService.java
@@ -0,0 +1,39 @@
+package com.rouvsen.springsecurityjwt.service;
+
+import com.rouvsen.springsecurityjwt.dao.UserRepository;
+import com.rouvsen.springsecurityjwt.exception.GeneralException;
+import com.rouvsen.springsecurityjwt.model.User;
+import com.rouvsen.springsecurityjwt.model.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserDto createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        var savedUser = userRepository.save(user);
+        return UserDto.builder()
+                .username(savedUser.getUsername())
+                .role(savedUser.getRole())
+                .email(savedUser.getEmail())
+                .build();
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> GeneralException.builder()
+                        .message("user not found by given id!")
+                        .status(HttpStatus.NOT_FOUND)
+                        .build());
+    }
+}
